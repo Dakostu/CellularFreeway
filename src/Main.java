a9de8a8c7cbd6dc800df37c875f41345a0a9e570
Dakostu <Dkostuj@gmx.de>
2017-11-28 22:25:05 +0100

    improved error handling

--- src/Main.java
+++ src/Main.java
@@ -20,35 +20,39 @@
 
 public class Main {
 
+    private static void printErrorExit(String message) {
+        System.err.println(message);
+        System.exit(1);
+    }
+
     public static void main(String[] args) throws IllegalArgumentException {
-
+        // we need at least four arguments
         if (args.length < 4)
-            throw new IllegalArgumentException
-                    ("\n\tNeed at least 4 arguments:\n\tfreeway length, " +
-                            "amount of cars, random decimal limit number," +
-                            " time interval in milliseconds)," + 
-                            " simulation steps (optional)");
+            printErrorExit("Usage: ./Java Main [length of street cells] " +
+                    "[amount of cars on street] [decimal probability number] " +
+                    "[time interval between simulation steps] [optional: amount of steps]");
 
 
-        //1st parameter: length of freeway (in slots)
+        //1st argument: length of freeway (in slots)
         int freewayLength = Integer.parseInt(args[0]);
-        //2nd parameter: amount of cars on freeway
+        //2nd argument: amount of cars on freeway
         int carsAmount = Integer.parseInt(args[1]);
-        //3rd parameter: value of random number propability limit
-        //4th parameter: time interval in milliseconds
+        //3rd argument: value of random number propability limit
+        //4th argument: time interval in milliseconds
         double probabilityLimit = Double.parseDouble(args[2]);
         long intervalms = Long.parseLong(args[3]);
         
 
         if (freewayLength < 1)
-            throw new IllegalArgumentException("Need a street of length bigger than 0");
+            printErrorExit("Need a street of length bigger than 0");
         else if (freewayLength < carsAmount || carsAmount < 0)
-            System.err.println("\nAmount of cars has to be a positive number " +
-                            "that is equal to or less than street length");
+            printErrorExit("Amount of cars has to be a positive number " +
+                    "that is equal to or less than street length");
         else if (probabilityLimit < 0 || probabilityLimit > 1)
-            throw new IllegalArgumentException("Random limit needs to be a decimal number between 0 and 1");
+            printErrorExit("Random limit needs to be a decimal number between 0 and 1");
         else if (intervalms < 0)
-            throw new IllegalArgumentException("Time interval needs to be a positive integer");
+            printErrorExit("Time interval needs to be a positive integer");
+
 
 
         Street freeway = new Street(freewayLength);
@@ -56,10 +60,11 @@ public class Main {
         if (args.length == 4) {
             freeway.simulateTraffic(probabilityLimit, intervalms);
         } else {
-            //5th parameter (optional): number of simulation steps
+            //5th argument (optional): number of simulation steps
             int simSteps = Integer.parseInt(args[4]);
-            if (simSteps < 0)
-                throw new IllegalArgumentException("Simulation step amount needs to be a positive number");
+            if (simSteps < 0) {
+                printErrorExit("Simulation step amount needs to be a positive integer");
+            }
             freeway.simulateTraffic(probabilityLimit, intervalms, simSteps);
         }
             
